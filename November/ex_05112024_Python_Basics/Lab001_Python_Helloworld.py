340842deb6a7c7f0c0d2d16349fde31fbb5b20ac
Jiragal <jiragal@gmail.com>
2024-11-18 13:08:54 +0530

    Initial commit

---
+++ November/ex_05112024_Python_Basics/Lab001_Python_Helloworld.py
@@ -0,0 +1,17 @@
+#Print command
+from keyword import kwlist
+print("Hello World!")
+print(3+2)
+print('hello',"numbr",'are you there',sep='*')
+sentence='hello world wellcome to Python'
+print(sentence)
+languages ='python','ruby','java','cobol','csharp','c&c++'
+print(languages)
+print(type(languages))
+print(type(sentence))
+import keyword
+print(keyword.kwlist)
+# keyword identifiers ---Reserved words, Python also has some reserved words
+# We cannot use them as a variable or in funstions, In python are case sensitives
+# Example class is a reserved word, udslly keywords are in blur color
+# Variable=Need to store the values
---
+++ November/ex_07112024_Variables_Kewords/Lab002_Python_Variables_types.py
@@ -0,0 +1,42 @@
+# Python determines the variable type during runtime rather than the compilation time
+# There are 3 types of variables in Pythonn 1)String 2)Numbers 3)Booleans
+from http.cookiejar import uppercase_escaped_char
+
+age = 99  # Integer
+pi = 3.14  # Decimal
+name = 'Pramod'  # String ---It is bunch of words or characters
+# Variable name always starts with (a-z, A-Z, 0-9)
+# An Underscore(_) followed by zeros or more letters
+# Underscores and digits(0-9)
+a = 40
+_ = 90
+_ = _ + 1
+print(_)
+is_active = True
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+users = {"name": 'Alice', 'age': 47}
+cordinates = (10, 20)
+a, b, c = 1, 2, 3
+e = 10 + 34 * 3 - 1
+print(e)
+# To find the max -prebuilt function
+result = max(33, 55, 35, 77, 88, 64, 99, 10)
+print(result)
+word = 'Hello world'
+print(len(word))
+name = 'Sanjivakumar'
+name = 10
+print(type(name))
+name = True
+print(type(name))
+name = 10.5
+print(type(name))
+
+txt = 'hello'
+print(txt.upper())
+txt = 'SANJIVAKUMAR'
+print(txt.lower())
+sentence = 'hello world hello hello hello universe hi there hi how are you doing'
+a = sentence.count("hello")
+print(a)
+print(txt.count('I'))
