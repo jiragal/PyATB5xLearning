ead7beed337aee59540b23c507d1b003f3569e42
Jiragal <jiragal@gmail.com>
2024-11-27 20:00:38 +0530

    save all the code

--- November/ex_23112024_Python_lists_tupple/Lab0018_Python_Lambda_Function.py
+++ November/ex_23112024_Python_lists_tupple/Lab0018_Python_Lambda_Function.py
@@ -3,27 +3,39 @@
 def add(a, b):
     return a + b
 
+
 def func(a, b):
     return a ** 2 + b ** 2 + 2 * a * b
+
 
 def func2(a, b, c):
     return 2 * a + 3 * b + 4 * c
 
+
 def last(item):
     return item[-1]
+
+
+def find_even_odd(num):
+    if num % 2 == 0:
+        print('The given number', num)
+    else:
+        print("The number is Odd")
+
 
 # For the above 4 Function now we will try to create Labmbda Functions
 add = lambda a, b: a + b
 print(add(100, 250))
 
-func = lambda a, b, c : a ** 2 + b ** 2 + 2 * a * b
-print(func(10,10,10))
+func = lambda a, b, c: a ** 2 + b ** 2 + 2 * a * b
+print(func(10, 10, 10))
 
-func2 = lambda a, b, c : 2 * a + 3 * b + 4 * c
-print(func2(10,10,10))
+func2 = lambda a, b, c: 2 * a + 3 * b + 4 * c
+print(func2(10, 10, 10))
 
-last = lambda item : item[-1:]
+last = lambda item: item[-1:]
 print(last('Pramod'))
 
-
-
+find_even_odd = lambda num: "even" if num % 2 == 0 else "The number is Odd"
+print(find_even_odd(99))
+print(find_even_odd(100))
